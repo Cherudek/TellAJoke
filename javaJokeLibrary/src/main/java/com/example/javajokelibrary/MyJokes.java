14109ea09c6f6a00abef8004a9bc19a706b97ae6
Gregorio <greg.massara@gmail.com>
2018-04-10 15:29:55 +0100

    Initial commit

---
+++ javaJokeLibrary/src/main/java/com/example/javajokelibrary/MyJokes.java
@@ -0,0 +1,11 @@
+package com.example.javajokelibrary;
+
+public class MyJokes {
+
+  public String tellAHandCraftedJoke(){
+
+    return "Of course I should clean my windows. But privacy is important too.";
+
+  }
+
+}
