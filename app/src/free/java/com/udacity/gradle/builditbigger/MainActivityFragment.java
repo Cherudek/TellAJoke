19cd47f4b5919a1aa21232f23b750248e19d5f94
Gregorio <greg.massara@gmail.com>
2018-04-23 14:52:35 +0100

    ProductFlavours Free and Paid added to App Module in the paid one the Add is removed

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,50 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+     public View root;
+
+    private ProgressBar progressBar;
+
+    public MainActivityFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        progressBar = getActivity().findViewById(R.id.progress_bar);
+
+        AdView mAdView = root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(getContext(), "ca-app-pub-3940256099942544~3347511713");
+
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+        return root;
+    }
+}
---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,86 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import com.example.androiddisplayjokeslibrary.DisplayJokeActivity;
+import com.udacity.gradle.builditbigger.EndpointsAsyncTask.AsyncTaskCallback;
+
+
+public class MainActivity extends AppCompatActivity implements
+    AsyncTaskCallback {
+
+  public Context mContext;
+  AsyncTaskCallback asyncTaskCallback = new AsyncTaskCallback() {
+    @Override
+    public void callBack(String joke) {
+
+    }
+  };
+  private String MAIN_ACTIVITY_LOG_TAG = MainActivity.class.getName();
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mContext = getApplicationContext();
+
+  }
+
+  @Override
+  public void callBack(String joke) {
+
+    Intent intent = new Intent(this, DisplayJokeActivity.class);
+    intent.putExtra(DisplayJokeActivity.JOKE_INTENT_TAG, joke);
+    this.startActivity(intent);
+
+    asyncTaskCallback.callBack(joke);
+
+    Log.i(MAIN_ACTIVITY_LOG_TAG, "TEST *** The Joke is: " + joke);
+
+
+  }
+
+  public void tellJoke(View view) {
+
+    EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(mContext);
+
+    endpointsAsyncTask.execute();
+
+    Toast.makeText(this, "The joke is on its way hold on tight!", Toast.LENGTH_SHORT)
+        .show();
+
+
+  }
+
+
+}
